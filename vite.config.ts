363bc123ffe1bc326e5f39b2cad599d37fb500ca
moi <jarodnyc@me.com>
2026-02-08 01:06:42 +0100

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-    export default defineConfig({
-  base: '/magique-liste/'
-})
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
+  base: "/magique-liste/",
+
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 }));
